d6134f003ec940766f81b0a6300526299736d459
Iulian Vasile Baba <iulian509@gmail.com>
2024-07-22 20:55:03 +0200

    chore: Update API dependencies and Python version to 3.12

--- src/api/common/dependencies.py
+++ src/api/common/dependencies.py
@@ -1,8 +1,10 @@
+from typing import AsyncIterator
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .database import async_session
 
 
-async def get_db() -> AsyncSession:
+async def get_db() -> AsyncIterator[AsyncSession]:
     async with async_session() as session:
         yield session
